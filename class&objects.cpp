4d40934935090d9884ed24753e6fe395756fdbd4
sukhi-mukhi <85299040+sukhi-mukhi@users.noreply.github.com>
2021-12-29 17:20:55 +0530

    Create class&objects.cpp

---
+++ class&objects.cpp
@@ -0,0 +1,16 @@
+// Class and Object
+#include <iostream>
+using namespace std;
+
+class Test{
+  public:
+    void display(){
+        cout<<"Hello..Welcome to world of object oriented programming!";
+    }
+};
+ int main(){
+     
+     Test t1;
+     t1.display();
+     return 0;
+ }
