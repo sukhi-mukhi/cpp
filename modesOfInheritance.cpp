9008ebbadd83ab0a2bdab925541b0a1004198abc
sukhi-mukhi <85299040+sukhi-mukhi@users.noreply.github.com>
2022-01-04 12:57:31 +0530

    Create modesOfInheritance.cpp

---
+++ modesOfInheritance.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************************
+
+ Modes of Inheritance 
+
+*******************************************************************************************/
+#include <iostream>
+using namespace std;
+
+class A{
+    public:
+        int x;
+    protected:
+        int y;
+    private:
+        int z;
+};
+
+class B: public A{
+    // x is public
+    // y is protected
+    // z is not accessible from B
+};
+
+class C: protected A{
+    // x is protected
+    // y is protected
+    // z is not accessible from C
+};
+
+class D: private A{
+    // x is private
+    // y is private
+    // z is not accessible from D
+};
